809be66d9a4d87df8714825a2d17a8befeaa08b0
JivkoJelev91 <zhelev.zhivko.1991@gmail.com>
2018-12-17 18:37:21 +0200

    first commit

---
+++ .history/src/Buttons/Commands_20181217183211.js
@@ -0,0 +1,66 @@
+// import styled from 'styled-components';
+import React, { Component}  from 'react';
+
+
+const CommandGrid  = {
+  
+}
+
+
+// function (command) {
+//   return function() {
+//     console.log(`Sending the command ${command}`);
+//     socket.emit('command', command);
+//   };
+// }
+
+const amount = 100;
+const Commands = () => (
+    <button className="rotate" onClick={console.log('works')}>
+      <span className="symbol">⟲</span> 90°
+    </button>
+    <button onClick={(`forward ${amount}`)}>
+      <span className="symbol">↑</span> forward {amount}cm
+    </button>
+    <button className="rotate" onClick={console.log('works')}>
+      <span className="symbol">⟳</span> 15°
+    </button>
+    <button onClick={(`left ${amount}`)}>
+      <span className="symbol">←</span> left {amount}cm
+    </button>
+    <div className="center">
+      <button className="takeoff" onClick={console.log('works')}>
+        Take Off
+      </button>
+      <button className="land" onClick={console.log('works')}>
+        Land
+      </button>
+      <button className="emergency" onClick={console.log('works')}>
+        !! emergency !!
+      </button>
+    </div>
+    <button onClick={console.log('works')}>
+      <span className="symbol">→</span>
+      right {amount}cm
+    </button>
+    <button className="height" onClick={console.log('works')}>
+      <span className="symbol">⤒</span> {amount}cm
+    </button>
+    <button onClick={console.log('works')}>
+      <span className="symbol">↓</span> back {amount}cm
+    </button>
+    <button className="height" onClick={console.log('works')}>
+      <span className="symbol">⤓</span> {amount}cm
+    </button>
+    <h2>Fancy Pants</h2>
+    <button onClick={console.log('works')}>Flip Left</button>
+    <button onClick={console.log('works')}>Flip Right</button>
+    <button onClick={console.log('works')}>Flip Back</button>
+    <button onClick={console.log('works')}>Flip Forward</button>
+    <button onClick={console.log('works')}>Go 25 25 25 25</button>
+    <button onClick={console.log('works')}>
+      Curve!
+    </button>
+);
+
+export default Commands;
